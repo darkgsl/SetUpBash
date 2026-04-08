9c07b2a7eda2b83ece8619fed0eb096305dadaaa
darkgsl <54315640+darkgsl@users.noreply.github.com>
2026-04-08 12:18:41 +0300

    Create install_aliases.sh

---
+++ install_aliases.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+cat >> ~/.bash_aliases << 'EOF'
+# Git aliases
+alias gs='git status'
+alias ga='git add'
+alias gc='git commit -m'
+alias gac='git add . && git commit -m'
+alias gp='git push'
+alias gl='git log --oneline --graph --all'
+alias gd='git diff'
+# Docker
+alias d=docker
+alias di="docker images"
+alias dps="docker ps"
+# k8s
+alias k='kubectl'
+alias kgp='kubectl get pods'
+EOF
+source ~/.bash_aliases
